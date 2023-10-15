120c70f47d8eeb75dcca65e623b49b32617eeb83
Arpit Verma <arpitv970@gmail.com>
2023-10-15 15:34:21 +0530

    updated user profile

--- components/sections/Brief.tsx
+++ components/sections/Brief.tsx
@@ -1,11 +1,24 @@
 import GenericPages from "@/utils/GenericPages"
+import Image from "next/image"
 
 const Brief = () => {
   return (
     <GenericPages title={'Brief Intro'} bgColor='lightGrey'>
-      <section className=''>
+      <section className='brief-intro-sec'>
         <p>I am a Freelancer, Full-Stack Developer & Open-Source Contributor. Also, I have great work experience in building products & services for various startups. Being a curious developer, I am currently exploring the vast field of DevOps in as full-time role.</p>
-        <section></section>
+        <section className='my-profile '>
+          <Image
+            src='/assets/images/Arpit-img.svg'
+            alt='Arpit Verma'
+            height={100}
+            width={100}
+          // fill
+          // style={{
+          //   objectFit: 'cover'
+          // }}
+          />
+          <p>arpitv970@gmail.com</p>
+        </section>
       </section>
       <section></section>
       <button></button>
---
+++ utils/index.js
@@ -0,0 +1,114 @@
+const iconPath = '/assets/icons/';
+const logoPath = '/assets/logo/';
+const images = '/assets/images/';
+
+export const user = {
+  name: 'Arpit Verma',
+  email: 'arpitv970@gmail.com',
+  phone: '+91 6375796873',
+  avatar: '/assets/images/Arpit-img.svg',
+  socials,
+  skills,
+  projects,
+  pricing,
+}
+
+const socials = [
+  {
+    insta: 'https://www.instagram.com/arpitv970/',
+    logo: `${iconPath}/insta-icon.svg`,
+  },
+  {
+    github: 'https://github.com/arpitv970/',
+    logo: `${iconPath}/github-icon.svg`,
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/arpitv970/',
+    logo: `${iconPath}/linkedin-icon.svg`,
+  },
+  {
+    twitter: 'https://twitter.com/arpitv970',
+    logo: `${iconPath}/twitter-icon.svg`,
+  },
+];
+
+const skills = [
+  {
+    skill: 'Next.js',
+    logo: `${logoPath}/nextjs-logo.svg`,
+  },
+  {
+    skill: 'JavaScript',
+    logo: `${logoPath}/js-logo.svg`,
+  },
+  {
+    skill: 'TypeScript',
+    logo: `${logoPath}/ts-logo.svg`,
+  },
+  {
+    skill: 'Express.js',
+    logo: `${logoPath}/expressjs-logo.svg`,
+  },
+  {
+    skill: 'TailwindCSS',
+    logo: `${logoPath}/tailwindcss-logo.svg`,
+  },
+  {
+    skill: 'React.js',
+    logo: `${logoPath}/reactjs-logo.svg`,
+  },
+  {
+    skill: 'Node.js',
+    logo: `${logoPath}/nodejs-logo.svg`,
+  },
+  {
+    skill: 'Postgres',
+    logo: `${logoPath}/psql-logo.svg`,
+  },
+  {
+    skill: 'MongoDB',
+    logo: `${logoPath}/mongodb-logo.svg`,
+  },
+  {
+    skill: 'Redux',
+    logo: `${logoPath}/redux-logo.svg`,
+  },
+];
+
+const projects = [
+  {
+    pName: '#Project',
+    pDesc: [
+      'Project Description',
+      'Project Description',
+      'Project Description',
+    ],
+  },
+];
+
+const pricing = [
+  {
+    price: '$10',
+    desc: [
+      'Static Site',
+      'Fully Responsive',
+      'Quick Delivery',
+    ],
+  },
+  {
+    price: '$10',
+    desc: [
+      'Static Site',
+      'Fully Responsive',
+      'Quick Delivery',
+    ],
+  },
+  {
+    price: '$10',
+    desc: [
+      'Static Site',
+      'Fully Responsive',
+      'Quick Delivery',
+    ],
+  },
+];
